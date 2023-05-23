373b1753e3e152178f9bb868dd53de0624d77781
Kreyster <manuel_vp789@hotmail.com>
2023-05-23 17:58:40 -0600

    se agrega una nueva id

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    '35.170.191.232',
+    '52.23.181.145',
 ]
 
 
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'Eshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cartas',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
